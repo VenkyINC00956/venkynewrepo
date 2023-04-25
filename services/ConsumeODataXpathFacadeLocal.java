e8f79411dfafde9d116ab3b9286abd3cbf5441a7
VenkyINC00956 <99635822+VenkyINC00956@users.noreply.github.com>
2023-04-25 11:28:39 +0530

    Delete services directory

--- services/ConsumeODataXpathFacadeLocal.java
+++
@@ -1,9 +0,0 @@
-package oneapp.incture.workbox.sharepoint.services;
-
-import oneapp.incture.workbox.poadapter.dto.ResponseMessage;
-
-public interface ConsumeODataXpathFacadeLocal {
-
-	ResponseMessage getDataFromECCNew(String processor, String scode);
-
-}
